e12dcd6ceb5b4a74434be1a9a1dbcf5e122eaf11
shujiangming <shujiangming@myhexin.com>
2026-01-05 04:33:21 -0800

    change

--- baseline/GRPO/run_grpo_search_r1_like.sh
+++ baseline/GRPO/run_grpo_search_r1_like.sh
@@ -57,7 +57,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768 \
-    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
