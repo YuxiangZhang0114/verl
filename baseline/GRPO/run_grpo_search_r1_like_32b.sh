e0fb8c42773536b97daa136c441665b839e63005
shujiangming <shujiangming@myhexin.com>
2026-01-06 18:51:33 -0800

    max_toke

--- baseline/GRPO/run_grpo_search_r1_like_32b.sh
+++ baseline/GRPO/run_grpo_search_r1_like_32b.sh
@@ -52,10 +52,10 @@ python3 -m verl.trainer.main_ppo \
     data.return_raw_chat=True \
     trainer.default_local_dir=$save_path \
     actor_rollout_ref.rollout.multi_turn.max_tool_response_length=10000 \
-    actor_rollout_ref.model.path=Qwen/Qwen2.5-32B-Instruct \
+    actor_rollout_ref.model.path=/model/32B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.actor.optim.lr_warmup_steps=2 \
-    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.use_remove_padding=True \g
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
