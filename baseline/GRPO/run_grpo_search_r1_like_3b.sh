59d5f683085d7802ce127a20fbdba2ee46f13bed
shujiangming <shujiangming@myhexin.com>
2026-01-06 23:09:01 -0800

    toolparser

--- baseline/GRPO/run_grpo_search_r1_like_3b.sh
+++ baseline/GRPO/run_grpo_search_r1_like_3b.sh
@@ -24,7 +24,7 @@
 set -x
 export RAY_NUM_CPUS=24
 export OMP_NUM_THREADS=1
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 # make sure your current working directory is the root of the project
 ulimit -n 65535
 export WANDB_API_KEY="0559d52399bc5d3fd8e373bb4b8b6e8db054b9f7"
