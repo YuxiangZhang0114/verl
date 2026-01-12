c17c8617b233da4f2756131cd636d8f3643419b5
shujiangming <shujiangming@myhexin.com>
2026-01-11 23:41:15 -0800

    Adjust batch sizes in run_prime_qwen_qa-freeze-T4S4.sh: set train_batch_size to 16, ppo_mini_batch_size to 16, and reward_model.mini_batch_size to 1 for improved training efficiency.

--- recipe/prime/run_prime_qwen_qa-freeze-T4S4.sh
+++ recipe/prime/run_prime_qwen_qa-freeze-T4S4.sh
@@ -31,7 +31,7 @@ save_path="/mnt/workspace/checkpoints/search_r1_like_grpo_sglang_qwen3-4b-instru
 python3 -m recipe.prime.main_prime \
     data.train_files="$train_files" \
     data.val_files="$test_files" \
-    data.train_batch_size=128 \
+    data.train_batch_size=16 \
     data.val_batch_size=200 \
     data.max_prompt_length=1024 \
     data.max_response_length=8192 \
@@ -44,7 +44,7 @@ python3 -m recipe.prime.main_prime \
     actor_rollout_ref.model.path=$model_path \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
@@ -74,7 +74,7 @@ python3 -m recipe.prime.main_prime \
     reward_model.model.optim.grad_clip=10.0 \
     +reward_model.model.ulysses_sequence_parallel_size=2 \
     reward_model.model.input_tokenizer=null \
-    reward_model.mini_batch_size=2 \
+    reward_model.mini_batch_size=1 \
     trainer.val_before_train=False \
     trainer.logger='["console","wandb"]' \
     trainer.project_name='search_r1_like_grpo_sglang' \
