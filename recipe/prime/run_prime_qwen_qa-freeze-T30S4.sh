c0e16b5fa4903b6372bf119f45f8721c5e1d5413
shujiangming <shujiangming@myhexin.com>
2026-01-12 02:36:53 -0800

    Remove PyTorch memory optimization settings from run_prime_qwen_qa-freeze-T30S4.sh to streamline the script and simplify memory management.

--- recipe/prime/run_prime_qwen_qa-freeze-T30S4.sh
+++ recipe/prime/run_prime_qwen_qa-freeze-T30S4.sh
@@ -31,12 +31,10 @@ save_path="/mnt/workspace/checkpoints/search_r1_like_grpo_sglang_qwen3-4b-instru
 python3 -m recipe.prime.main_prime \
     data.train_files="$train_files" \
     data.val_files="$test_files" \
-    data.train_batch_size=64 \
+    data.train_batch_size=128 \
     data.val_batch_size=200 \
     data.max_prompt_length=1024 \
     data.max_response_length=10240 \
-    actor_rollout_ref.actor.ulysses_sequence_parallel_size=4 \
-    actor_rollout_ref.ref.ulysses_sequence_parallel_size=4 \
     data.return_raw_chat=True \
     data.filter_overlong_prompts=True \
     data.filter_accuracy=True \
@@ -44,9 +42,11 @@ python3 -m recipe.prime.main_prime \
     data.accuracy_upper_bound=1.0 \
     data.oversample_factor=1 \
     actor_rollout_ref.model.path=$model_path \
+    actor_rollout_ref.actor.ulysses_sequence_parallel_size=4 \
+    actor_rollout_ref.ref.ulysses_sequence_parallel_size=4 \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
@@ -54,7 +54,7 @@ python3 -m recipe.prime.main_prime \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.multi_turn.tool_config_path="$TOOL_CONFIG" \
     actor_rollout_ref.rollout.multi_turn.enable=True \
-    actor_rollout_ref.rollout.multi_turn.max_assistant_turns=15 \
+    actor_rollout_ref.rollout.multi_turn.max_assistant_turns=16 \
     actor_rollout_ref.rollout.multi_turn.format=hermes \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
