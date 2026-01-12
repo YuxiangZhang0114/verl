979d350797fecd4909472ca0cc3df8ea0f4acb78
shujiangming <shujiangming@myhexin.com>
2026-01-11 20:33:29 -0800

    可运行版本的prime： Add new scripts for running Qwen models with math data: run_prime_qwen_math_freeze_T4S4.sh and run_prime_qwen_math_freeze_T30S4.sh. Configure training parameters and paths for GSM8K and math datasets, optimizing for different model sizes.

---
+++ recipe/prime/run_prime_qwen_math_freeze_T30S4.sh
@@ -0,0 +1,83 @@
+set -x
+
+
+gsm8k_train_path=$HOME/data/gsm8k/train.parquet
+gsm8k_test_path=$HOME/data/gsm8k/test.parquet
+export WANDB_API_KEY="0559d52399bc5d3fd8e373bb4b8b6e8db054b9f7"
+
+# download from https://huggingface.co/datasets/PRIME-RL/Eurus-2-RL-Data
+math_train_path=$HOME/data/math/train.parquet
+math_test_path=$HOME/data/math/test.parquet
+
+train_files="['$math_train_path']"
+test_files="['$gsm8k_test_path']"
+
+# model_path=PRIME-RL/Eurus-2-7B-SFT
+model_path=Qwen/Qwen3-4B-Instruct-2507
+reward_model_path=Qwen/Qwen3-30B-A3B-Instruct-2507
+
+
+# TRAIN_DATA="$PROJECT_DIR/data/asearcher_train/train.parquet"
+# VAL_DATA="$PROJECT_DIR/data/hotpotqa_hard_train/validation.parquet"
+
+# train_files="['$TRAIN_DATA']"
+# test_files="['$VAL_DATA']"
+# Tool config path
+
+
+save_path="/mnt/workspace/checkpoints/search_r1_like_grpo_sglang_qwen3-4b-instruct_asearcher"
+
+
+python3 -m recipe.prime.main_prime \
+    data.train_files="$train_files" \
+    data.val_files="$test_files" \
+    data.train_batch_size=64 \
+    data.val_batch_size=6312 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=4096 \
+    data.return_raw_chat=True \
+    data.filter_overlong_prompts=True \
+     data.return_raw_chat=True \
+    actor_rollout_ref.rollout.multi_turn.enable=False \
+    data.filter_accuracy=True \
+    data.accuracy_lower_bound=0.0 \
+    data.accuracy_upper_bound=1.0 \
+    data.oversample_factor=1 \
+    actor_rollout_ref.model.path=$model_path \
+    actor_rollout_ref.actor.optim.lr=5e-7 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.name=sglang \
+    actor_rollout_ref.rollout.mode=async \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.n=5 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.rollout.agent.default_agent_loop=single_turn_agent \
+    algorithm.adv_estimator=grpo \
+    algorithm.use_kl_in_reward=False \
+    algorithm.kl_penalty=kl \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    reward_model.model.path=$reward_model_path \
+    reward_model.micro_batch_size_per_gpu=1 \
+    reward_model.model.update=none \
+    reward_model.model.beta_train=0.05 \
+    reward_model.model.optim.lr=1e-6 \
+    reward_model.model.optim.grad_clip=10.0 \
+    reward_model.model.input_tokenizer=null \
+    reward_model.mini_batch_size=64 \
+    trainer.val_before_train=False \
+    trainer.logger='["console","wandb"]' \
+    trainer.project_name='prime_example' \
+    trainer.experiment_name='Qwen3-4B-Instruct-gsm8k' \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=64 \
+    trainer.test_freq=64 \
+    trainer.total_epochs=1 $@
