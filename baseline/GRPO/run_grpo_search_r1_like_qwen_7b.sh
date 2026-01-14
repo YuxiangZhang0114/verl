2c7455bd1f38e785001da3f4248b45d3b636276d
shujiangming <shujiangming@myhexin.com>
2026-01-13 19:12:56 -0800

    Update max_tool_response_length in GRPO search script from 10000 to 8192 for optimized performance.

--- baseline/GRPO/run_grpo_search_r1_like_qwen_7b.sh
+++ baseline/GRPO/run_grpo_search_r1_like_qwen_7b.sh
@@ -44,14 +44,14 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_batch_size=64 \
     data.val_batch_size=400 \
-    data.max_prompt_length=2048 \
+    data.max_prompt_length=1024 \
     data.max_response_length=20480 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.return_raw_chat=True \
     actor_rollout_ref.actor.checkpoint.save_contents=['hf_model'] \
     trainer.default_local_dir=$save_path \
-    actor_rollout_ref.rollout.multi_turn.max_tool_response_length=10000 \
+    actor_rollout_ref.rollout.multi_turn.max_tool_response_length=8192 \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-7B-Instruct\
     actor_rollout_ref.actor.optim.lr=5e-7 \
     actor_rollout_ref.actor.optim.lr_warmup_steps=0 \
